df74aa3d5d7eabdaeb9c2282549ac189ab217a77
At <atbspb@qq.com>
2026-05-19 00:07:41 +0800

    Footer scroll fade effect - transparent when covering content, fade in when scrolling to bottom

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,7 +1,7 @@
 Copyright (c) {{ currentYear }} ATBSPB. Licensed under the MIT License.
 
 <template>
-  <footer>
+  <footer :style="{ opacity: footerOpacity }">
     Copyright &copy; {{ currentYear }} ATBSPB. All rights reserved.
     <br />
     Powered by <a href="https://www.netlify.com" target="_blank">Netlify</a> &amp; <a href="https://zyyo.net/" target="_blank">zyyo</a>
@@ -9,7 +9,34 @@ Copyright (c) {{ currentYear }} ATBSPB. Licensed under the MIT License.
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 const currentYear = ref(new Date().getFullYear())
+const footerOpacity = ref(0)
+
+const FADE_THRESHOLD = 300
+
+function handleScroll() {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop
+  const windowHeight = window.innerHeight
+  const documentHeight = document.documentElement.scrollHeight
+  const distanceFromBottom = documentHeight - (scrollTop + windowHeight)
+
+  if (distanceFromBottom <= 0) {
+    footerOpacity.value = 1
+  } else if (distanceFromBottom >= FADE_THRESHOLD) {
+    footerOpacity.value = 0
+  } else {
+    footerOpacity.value = 1 - distanceFromBottom / FADE_THRESHOLD
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll, { passive: true })
+  handleScroll()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
