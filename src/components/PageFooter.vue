a5e84acbcb599bf24440358abc6099f066aa7d8c
At <atbspb@qq.com>
2026-05-23 03:32:12 +0800

    Remove powered by links from footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,8 +3,6 @@ Copyright (c) {{ currentYear }} ATBSPB. Licensed under the MIT License.
 <template>
   <footer :style="{ opacity: footerOpacity }">
     Copyright &copy; {{ currentYear }} ATBSPB. All rights reserved.
-    <br />
-    Powered by <a href="https://www.netlify.com" target="_blank">Netlify</a> &amp; <a href="https://zyyo.net/" target="_blank">zyyo</a>
   </footer>
 </template>
 
